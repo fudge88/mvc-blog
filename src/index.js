2a9159020f5995bf901cd2725b9c774756e71510
Fahra Akhlaq <f.akhlaq@outlook.com>
2021-12-30 22:38:02 +0000

    sequelizestore not working

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+require("dotenv").config();
+
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
 const path = require("path");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const connection = require("./config/connection");
+const { sequelize } = require("./models/blog");
 
 const routes = require("./routes");
 
 const PORT = process.env.PORT || 4000;
+
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {
+    maxAge: 86400,
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
 const hbs = expressHandlebars.create({});
 const app = express();
@@ -14,6 +31,7 @@ const app = express();
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
+app.use(session(sessionOptions));
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
