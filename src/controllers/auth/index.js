5278078092738f5b2268adbf6882a5a9c668d17b
Fahra Akhlaq <f.akhlaq@outlook.com>
2021-12-30 20:00:30 +0000

    password check created and works

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -1,8 +1,41 @@
 const { getPayloadWithValidFieldsOnly } = require("../../helpers");
 const { User } = require("../../models");
 
-const login = (req, res) => {
-  res.render("login");
+const login = async (req, res) => {
+  try {
+    const payload = getPayloadWithValidFieldsOnly(
+      ["email", "password"],
+      req.body
+    );
+    if (Object.keys(payload).length !== 2) {
+      return res
+        .status(400)
+        .json({ success: false, error: "please complete all fields" });
+    }
+
+    const user = await User.findOne({ where: { email: payload.email } });
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, error: "User does not exist" });
+    }
+
+    const validPassword = await user.checkPassword(payload.password);
+
+    if (!validPassword) {
+      return res
+        .status(401)
+        .json({ success: false, error: "User Password incorrect" });
+    }
+
+    return res.json({ success: true, data: "Login Successful" });
+  } catch (error) {
+    console.log(`[ERROR]: Login user failed | ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to login user" });
+  }
 };
 
 const signup = async (req, res) => {
