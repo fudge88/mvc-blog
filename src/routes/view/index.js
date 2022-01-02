e1284fdc6fbb5d7ab03b30858d3683c30ed389c7
Fahra Akhlaq <f.akhlaq@outlook.com>
2022-01-02 18:53:50 +0000

    auth middleware added

--- src/routes/view/index.js
+++ src/routes/view/index.js
@@ -2,10 +2,11 @@ const { Router } = require("express");
 
 const publicRoutes = require("./public-routes");
 const privateRoutes = require("./private-routes");
+const auth = require("../../middleware/auth");
 
 const router = Router();
 
 router.use(publicRoutes);
-router.use(privateRoutes);
+router.use(auth, privateRoutes);
 
 module.exports = router;
