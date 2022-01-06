b13e7a5fe15a2e8adcbfa92bae1547f8ce5f8aa0
Fahra Akhlaq <f.akhlaq@outlook.com>
2022-01-06 19:51:15 +0000

    changed es6 function to normal fucntion - bug fixed- data being rendered

--- src/helpers/hbs-helpers.js
+++ src/helpers/hbs-helpers.js
@@ -1,11 +1,9 @@
-const staggeredDiv = (conditional, options) => {
-  if (conditional % 2 == 0) {
-    return options.fn(this);
-  } else {
-    return options.inverse(this);
-  }
-};
-
 module.exports = {
-  staggeredDiv,
+  staggeredDiv: function (conditional, options) {
+    if (conditional % 2 == 0) {
+      return options.fn(this);
+    } else {
+      return options.inverse(this);
+    }
+  },
 };
--- src/seeds/blog-seeds.js
+++ src/seeds/blog-seeds.js
@@ -3,16 +3,22 @@ const Blog = require("../models/Blog");
 const blogData = [
   {
     user_id: 3,
+    image:
+      "https://images.unsplash.com/photo-1518006158933-f03b3dbb5ef8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fG1vdW50YWluJTIwdHJla3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
     title: "blog 1",
     blog: "This is blog 1",
   },
   {
     user_id: 2,
+    image:
+      "https://images.unsplash.com/photo-1525856897812-33d00556e714?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fG1vdW50YWluJTIwdHJla3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
     title: "blog 2",
     blog: "This is blog 2",
   },
   {
     user_id: 1,
+    image:
+      "https://images.unsplash.com/photo-1619456613769-1cd2c8f6ea90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fG1vdW50YWluJTIwdHJla3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
     title: "blog 3",
     blog: "This is blog 3",
   },
